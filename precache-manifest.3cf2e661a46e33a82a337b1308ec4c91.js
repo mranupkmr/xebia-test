3efb8e8b35290b79caeb0d73aa121916d2c6b879
mranupkmr <mr.anupkmr@gmail.com>
2019-07-14 02:16:34 +0530

    Updates

---
+++ precache-manifest.3cf2e661a46e33a82a337b1308ec4c91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e88d660a4db6bb07a8395d37f38347c",
+    "url": "/mranupkmr/xebia-test/index.html"
+  },
+  {
+    "revision": "4c389692b7255032abca",
+    "url": "/mranupkmr/xebia-test/static/css/main.3c446c7d.chunk.css"
+  },
+  {
+    "revision": "f743d9114736ebff6a74",
+    "url": "/mranupkmr/xebia-test/static/js/2.8aa63c7f.chunk.js"
+  },
+  {
+    "revision": "4c389692b7255032abca",
+    "url": "/mranupkmr/xebia-test/static/js/main.fef748a9.chunk.js"
+  },
+  {
+    "revision": "de7559fc2cca373b347f",
+    "url": "/mranupkmr/xebia-test/static/js/runtime~main.0703069c.js"
+  }
+]);
